92fe765e64225f620f2fe11e6224a1009af587e3
EyobMengsteab <eyobmengsteab7@gmail.com>
2026-09-13 16:21:11 +0200

    Finilised logic structures and User interfaces

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -5,18 +5,18 @@ const questions = [
     'Which time complexity describes an algorithm whose '
         'running time doubles when the input size doubles?',
     [
+      'O(n)',
       'O(1)',
       'O(log n)',
-      'O(n)',
       'O(n²)',
     ],
   ),
   QuizQuestion(
-    'Which data structure is most commonly used '
-        'to implement a Breadth‑First Search (BFS)?',
+    'Which data structure is most commonly used to '
+        'implement a Breadth‑First Search (BFS)?',
     [
-      'Stack',
       'Queue',
+      'Stack',
       'Heap',
       'Linked List',
     ],
@@ -24,8 +24,8 @@ const questions = [
   QuizQuestion(
     'What is the main advantage of using a divide‑and‑conquer algorithm?',
     [
-      'It always produces the fastest solution',
       'It reduces the problem size by half at each step',
+      'It always produces the fastest solution',
       'It avoids recursion',
       'It guarantees constant‑time performance',
     ],
@@ -33,9 +33,9 @@ const questions = [
   QuizQuestion(
     'Which sorting algorithm has the best average‑case time complexity?',
     [
+      'Merge Sort',
       'Bubble Sort',
       'Insertion Sort',
-      'Merge Sort',
       'Selection Sort',
     ],
   ),
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectAnswer
+  });
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -15,7 +21,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++;
     });
@@ -36,14 +43,21 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             Text(
               currentQuestion.text,
 //            'The Question...',
-              style: const TextStyle(
-                color: Colors.white,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 237, 223, 252),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(answerText: answer, onTap: answerQuestion,);
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQuestion(answer);
+                },
+              );
             })
           ],
         ),
